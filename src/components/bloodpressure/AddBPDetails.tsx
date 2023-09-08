f8f9d3584a8d6894b0e1d5d54c38f98fafea4c6b
Urooj Ahmad <uroojahamad786@gmail.com>
2023-09-08 17:28:31 +0530

    created components for blood pressure page

--- src/components/bloodpressure/AddBPDetails.tsx
+++ src/components/bloodpressure/AddBPDetails.tsx
@@ -1,7 +1,130 @@
-import React from "react";
+import { Reading } from "@/app/page";
+import { supabase } from "@/supabase/client";
+import React, { Dispatch, SetStateAction, useState } from "react";
 
-const AddBPDetails = () => {
-  return <></>;
+type AddBPDetailsProps = {
+  setBpReading: Dispatch<SetStateAction<Reading[]>>;
+  lastID: number;
+};
+
+const AddBPDetails = ({ setBpReading, lastID }: AddBPDetailsProps) => {
+  const [inputState, setInputState] = useState<
+    Omit<Reading, "id" | "created_at">
+  >({
+    systolic: 0,
+    diastolic: 0,
+    pulse: 0,
+    arm: "Left",
+  });
+
+  //Get user input from fields
+  const handleChange = (e: any) => {
+    setInputState((prevInputState) => {
+      return { ...prevInputState, [e.target.name]: e.target.value };
+    });
+  };
+
+  //Insert data into supabase
+  const insertBPData = async (reading: Reading) => {
+    await supabase.from("bp").insert([reading]);
+  };
+
+  //Submit the details
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (
+      inputState.systolic === 0 ||
+      inputState.diastolic === 0 ||
+      inputState.pulse === 0
+    ) {
+      return alert("Enter all reading to add the details.");
+    }
+
+    const currentReading = {
+      id: lastID + 1,
+      created_at: new Date(),
+      ...inputState,
+    };
+
+    setBpReading((prevReading: Reading[]) => {
+      return [currentReading, ...prevReading];
+    });
+
+    setInputState({
+      systolic: 0,
+      diastolic: 0,
+      pulse: 0,
+      arm: "Left",
+    });
+
+    insertBPData(currentReading);
+  };
+  return (
+    <>
+      <div>
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <input
+            className="border border-black p-2 max-w-lg"
+            type="number"
+            name="systolic"
+            placeholder="Enter Systolic Pressure"
+            value={inputState.systolic || ""}
+            onChange={handleChange}
+          />
+          <input
+            className="border border-black p-2 max-w-lg"
+            type="number"
+            name="diastolic"
+            placeholder="Enter Diastolic Pressure"
+            value={inputState.diastolic || ""}
+            onChange={handleChange}
+          />
+          <input
+            className="border border-black p-2 max-w-lg"
+            type="number"
+            name="pulse"
+            placeholder="Enter Pulse reading"
+            value={inputState.pulse || ""}
+            onChange={handleChange}
+          />
+          <div className="flex gap-3">
+            <button
+              type="button"
+              className={`border ${
+                inputState.arm === "Left"
+                  ? "border-blue-800 bg-blue-100"
+                  : "border-gray-700 bg-gray-100"
+              }  outline-2 p-3 w-52 rounded-full text-xl`}
+              name="arm"
+              value="Left"
+              onClick={handleChange}
+            >
+              Left Arm
+            </button>
+            <button
+              type="button"
+              className={`border ${
+                inputState.arm === "Right"
+                  ? "border-blue-800 bg-blue-100"
+                  : "border-gray-700 bg-gray-100"
+              }  outline-2 p-3 w-52 rounded-full text-xl`}
+              name="arm"
+              value="Right"
+              onClick={handleChange}
+            >
+              Right Arm
+            </button>
+          </div>
+          <button
+            type="submit"
+            className="border border-black p-2 max-w-md bg-blue-600 text-white"
+          >
+            Add Readings
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default AddBPDetails;
--- src/components/bloodpressure/DisplayBPDetails.tsx
+++ src/components/bloodpressure/DisplayBPDetails.tsx
@@ -1,9 +1,47 @@
+import { Reading } from "@/app/page";
+import dayjs from "dayjs";
 import React from "react";
 
-type Props = {};
+type DisplayBPDetailsProps = {
+  bpReading: Reading[];
+};
 
-const DisplayBPDetails = (props: Props) => {
-  return <div>DisplayBPDetails</div>;
+const DisplayBPDetails = ({ bpReading }: DisplayBPDetailsProps) => {
+  return (
+    <div>
+      {bpReading.length > 0 && (
+        <table className="min-w-fit text-left text-sm font-light mt-5">
+          <thead className="border-b font-medium dark:border-neutral-500">
+            <tr>
+              <th className="px-6 py-4 text-xl">Date/Time</th>
+              <th className="px-6 py-4 text-xl">Systolic Pressure</th>
+              <th className="px-6 py-4 text-xl">Diastolic Pressure</th>
+              <th className="px-6 py-4 text-xl">Pulse Rate</th>
+              <th className="px-6 py-4 text-xl">Arm</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bpReading.map((reading) => {
+              return (
+                <tr
+                  className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600"
+                  key={reading.id}
+                >
+                  <td className="px-6 py-4 text-lg">
+                    {dayjs(reading.created_at).format("YYYY/MM/DD hh:mm A")}
+                  </td>
+                  <td className="px-6 py-4 text-lg">{reading.systolic}</td>
+                  <td className="px-6 py-4 text-lg">{reading.diastolic}</td>
+                  <td className="px-6 py-4 text-lg">{reading.pulse}</td>
+                  <td className="px-6 py-4 text-lg">{reading.arm}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 };
 
 export default DisplayBPDetails;
