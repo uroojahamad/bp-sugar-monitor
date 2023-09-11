40689f6bf77633e857813d05bd4bef7feada2089
Urooj Ahmad <uroojahamad786@gmail.com>
2023-09-11 16:09:32 +0530

    change modal box component to accept child element

--- src/components/modalbox/ModalBox.tsx
+++ src/components/modalbox/ModalBox.tsx
@@ -1,16 +1,11 @@
 import { useEffect } from "react";
 
 type ModalBoxProps = {
-  heading: string;
-  description: string;
+  children: any;
   onClose: () => void;
 };
 
-const ModalBox = ({ heading, description, onClose }: ModalBoxProps) => {
-  const onButtonClose = () => {
-    onClose();
-  };
-
+const ModalBox = ({ children, onClose }: ModalBoxProps) => {
   const onBackdropClose = (e: any) => {
     if (e.target.closest("#innerbox")) {
       return;
@@ -38,26 +33,7 @@ const ModalBox = ({ heading, description, onClose }: ModalBoxProps) => {
       className="flex justify-center items-center w-full h-full absolute top-0 bg-gray-900 bg-opacity-75"
       onClick={onBackdropClose}
     >
-      <div
-        id="innerbox"
-        className="z-50 border border-black w-1/3 h-72 flex flex-col gap-2 justify-center items-center bg-slate-200 rounded-xl"
-      >
-        <h1 className="text-2xl font-bold p-2 font-mono">{heading}</h1>
-        <p className="text-center p-2 text-xl max-w-lg font-mono">
-          {description}
-        </p>
-        <div className="mt-3 p-2 w-4/5 flex gap-2 justify-center items-center">
-          <button
-            className="border border-black w-64 h-14 rounded-lg text-xl cursor-pointer hover:bg-slate-300 font-mono font-semibold"
-            onClick={onButtonClose}
-          >
-            Cancel
-          </button>
-          <button className="border-none w-64 h-14 rounded-lg font-mono font-semibold bg-purple-700 text-white text-xl cursor-pointer hover:bg-purple-900">
-            Open Email App
-          </button>
-        </div>
-      </div>
+      {children}
     </div>
   );
 };
