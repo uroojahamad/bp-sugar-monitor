ae331b36fe2ce6ade2141fe7cb38492e7eb7670c
Urooj Ahmad <uroojahamad786@gmail.com>
2023-09-12 15:36:45 +0530

    added modal close function when adding new data

--- src/components/bloodpressure/AddBPDetails.tsx
+++ src/components/bloodpressure/AddBPDetails.tsx
@@ -5,9 +5,10 @@ import React, { Dispatch, SetStateAction, useState } from "react";
 type AddBPDetailsProps = {
   setBpReading: Dispatch<SetStateAction<Reading[]>>;
   lastID: number;
+  onClose: () => void;
 };
 
-const AddBPDetails = ({ setBpReading, lastID }: AddBPDetailsProps) => {
+const AddBPDetails = ({ setBpReading, lastID, onClose }: AddBPDetailsProps) => {
   const [inputState, setInputState] = useState<
     Omit<Reading, "id" | "created_at">
   >({
@@ -57,71 +58,86 @@ const AddBPDetails = ({ setBpReading, lastID }: AddBPDetailsProps) => {
       arm: "Left",
     });
 
+    //Modal box close
+    onClose();
+
     insertBPData(currentReading);
   };
   return (
     <>
-      <div>
-        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-          <input
-            className="border border-black p-2 max-w-lg"
-            type="number"
-            name="systolic"
-            placeholder="Enter Systolic Pressure"
-            value={inputState.systolic || ""}
-            onChange={handleChange}
-          />
-          <input
-            className="border border-black p-2 max-w-lg"
-            type="number"
-            name="diastolic"
-            placeholder="Enter Diastolic Pressure"
-            value={inputState.diastolic || ""}
-            onChange={handleChange}
-          />
-          <input
-            className="border border-black p-2 max-w-lg"
-            type="number"
-            name="pulse"
-            placeholder="Enter Pulse reading"
-            value={inputState.pulse || ""}
-            onChange={handleChange}
-          />
-          <div className="flex gap-3">
-            <button
-              type="button"
-              className={`border ${
-                inputState.arm === "Left"
-                  ? "border-blue-800 bg-blue-100"
-                  : "border-gray-700 bg-gray-100"
-              }  outline-2 p-3 w-52 rounded-full text-xl`}
-              name="arm"
-              value="Left"
-              onClick={handleChange}
-            >
-              Left Arm
-            </button>
-            <button
-              type="button"
-              className={`border ${
-                inputState.arm === "Right"
-                  ? "border-blue-800 bg-blue-100"
-                  : "border-gray-700 bg-gray-100"
-              }  outline-2 p-3 w-52 rounded-full text-xl`}
-              name="arm"
-              value="Right"
-              onClick={handleChange}
-            >
-              Right Arm
-            </button>
-          </div>
+      <div
+        id="innerbox"
+        className="z-50 border border-black w-72 md:w-2/5 h-fit py-3 flex flex-col gap-2 justify-center items-center bg-slate-200 rounded-xl"
+      >
+        <h1 className="text-2xl font-bold p-2 font-mono text-center">
+          Add Blood Pressure Reading
+        </h1>
+        <input
+          type="number"
+          className="w-3/4 p-4 border border-gray-300 rounded-md placeholder:font-mono placeholder:text-xs md:placeholder:text-sm"
+          name="systolic"
+          placeholder="Enter Systolic Pressure"
+          value={inputState.systolic || ""}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          className="w-3/4 p-4 border border-gray-300 rounded-md placeholder:font-mono placeholder:text-xs md:placeholder:text-sm"
+          name="diastolic"
+          placeholder="Enter Diastolic Pressure"
+          value={inputState.diastolic || ""}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          className="w-3/4 p-4 border border-gray-300 rounded-md placeholder:font-mono placeholder:text-xs md:placeholder:text-sm"
+          name="pulse"
+          placeholder="Enter Pulse reading"
+          value={inputState.pulse || ""}
+          onChange={handleChange}
+        />
+        <div className="flex gap-3 mt-5">
           <button
-            type="submit"
-            className="border border-black p-2 max-w-md bg-blue-600 text-white"
+            type="button"
+            className={`border ${
+              inputState.arm === "Left"
+                ? "border-blue-800 bg-blue-100"
+                : "border-gray-700 bg-gray-100 hover:bg-green-300"
+            }  outline-2 p-2 w-28 md:w-32 lg:w-40 rounded-full text-md font-mono`}
+            name="arm"
+            value="Left"
+            onClick={handleChange}
           >
-            Add Readings
+            Left Arm
           </button>
-        </form>
+          <button
+            type="button"
+            className={`border ${
+              inputState.arm === "Right"
+                ? "border-blue-800 bg-blue-100"
+                : "border-gray-700 bg-gray-100 hover:bg-green-300"
+            }  outline-2 p-2 w-28 md:w-32 lg:w-40 rounded-full text-md font-mono`}
+            name="arm"
+            value="Right"
+            onClick={handleChange}
+          >
+            Right Arm
+          </button>
+        </div>
+        <div className="mt-3 p-2 w-4/5 flex flex-col lg:flex-row gap-2 justify-center items-center">
+          <button
+            className="w-full flex justify-center items-center p-2 space-x-4 font-sans font-bold text-white rounded-md shadow-lg px-9 md:px-0 bg-red-500 shadow-cyan-100 hover:bg-opacity-90 hover:shadow-lg border transition hover:-translate-y-0.5 duration-150"
+            onClick={onClose}
+          >
+            Cancel
+          </button>
+          <button
+            className="w-full flex justify-center items-center p-2 space-x-4 font-sans font-bold text-white rounded-md shadow-lg px-9 md:px-0 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90 hover:shadow-lg border transition hover:-translate-y-0.5 duration-150"
+            onClick={handleSubmit}
+          >
+            Add Record
+          </button>
+        </div>
       </div>
     </>
   );
--- src/components/bloodsugar/AddBloodSugarDetails.tsx
+++ src/components/bloodsugar/AddBloodSugarDetails.tsx
@@ -52,27 +52,30 @@ const AddBloodSugarDetails = ({
       measure: "Before Meal",
     });
 
-    // insertSugarData(currentReading);
+    //Modalbox close
+    onClose();
+
+    insertSugarData(currentReading);
   };
 
   return (
     <>
       <div
         id="innerbox"
-        className="z-50 border border-black w-2/5 h-72 flex flex-col gap-2 justify-center items-center bg-slate-200 rounded-xl"
+        className="z-50 border border-black w-72 md:w-2/5 h-fit py-3 flex flex-col gap-2 justify-center items-center bg-slate-200 rounded-xl"
       >
-        <h1 className="text-2xl font-bold p-2 font-mono">
+        <h1 className="text-2xl font-bold p-2 font-mono text-center">
           Add Blood Sugar Reading
         </h1>
         <input
           type="number"
-          className="w-3/4 p-4 border border-gray-300 rounded-md placeholder:font-mono"
+          className="w-3/4 p-4 border border-gray-300 rounded-md placeholder:font-mono placeholder:text-xs md:placeholder:text-sm"
           name="sugar_level"
           placeholder="Enter Blood Sugar Level"
           value={inputState.sugar_level || ""}
           onChange={handleChange}
         />
-        <div className="flex gap-3 mt-5">
+        <div className="flex gap-3 mt-5 flex-wrap xl:flex-nowrap justify-center items-center">
           <button
             type="button"
             className={`border ${
@@ -126,94 +129,21 @@ const AddBloodSugarDetails = ({
             Fasting
           </button>
         </div>
-        <div className="mt-3 p-2 w-4/5 flex gap-2 justify-center items-center">
+        <div className="mt-3 p-2 w-4/5 flex flex-col lg:flex-row gap-2 justify-center items-center">
           <button
-            className="w-full flex justify-center items-center p-2 space-x-4 font-sans font-bold text-white rounded-md shadow-lg px-9 bg-red-500 shadow-cyan-100 hover:bg-opacity-90 hover:shadow-lg border transition hover:-translate-y-0.5 duration-150"
+            className="w-full flex justify-center items-center p-2 space-x-4 font-sans font-bold text-white rounded-md shadow-lg px-9 md:px-0 bg-red-500 shadow-cyan-100 hover:bg-opacity-90 hover:shadow-lg border transition hover:-translate-y-0.5 duration-150"
             onClick={onClose}
           >
             Cancel
           </button>
-          <button className="w-full flex justify-center items-center p-2 space-x-4 font-sans font-bold text-white rounded-md shadow-lg px-9 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90 hover:shadow-lg border transition hover:-translate-y-0.5 duration-150">
+          <button
+            className="w-full flex justify-center items-center p-2 space-x-4 font-sans font-bold text-white rounded-md shadow-lg px-9 md:px-0 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90 hover:shadow-lg border transition hover:-translate-y-0.5 duration-150"
+            onClick={handleSubmit}
+          >
             Add Record
           </button>
         </div>
       </div>
-
-      {/* <div className="border w-2/4 mx-auto p-3 flex justify-center items-center shadow-md sm:rounded-lg">
-        <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
-          <h2 className="text-xl font-bold text-center font-sans">
-            Blood Sugar Records
-          </h2>
-          <input
-            className="p-3 text-md border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block min-w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:font-sans"
-            type="number"
-            name="sugar_level"
-            placeholder="Enter Blood Sugar Level"
-            value={inputState.sugar_level || ""}
-            onChange={handleChange}
-          />
-          <div className="flex gap-3">
-            <button
-              type="button"
-              className={`border ${
-                inputState.measure === "Before Meal"
-                  ? "border-blue-800 bg-blue-100"
-                  : "border-gray-700 bg-gray-100 hover:bg-green-200"
-              }  outline-2 p-1 w-32 rounded-full text-md font-sans`}
-              name="measure"
-              value="Before Meal"
-              onClick={handleChange}
-            >
-              Before Meal
-            </button>
-            <button
-              type="button"
-              className={`border ${
-                inputState.measure === "After Meal"
-                  ? "border-blue-800 bg-blue-100"
-                  : "border-gray-700 bg-gray-100 hover:bg-green-200"
-              }  outline-2 p-1 w-32 rounded-full text-md font-sans`}
-              name="measure"
-              value="After Meal"
-              onClick={handleChange}
-            >
-              After Meal
-            </button>
-            <button
-              type="button"
-              className={`border ${
-                inputState.measure === "At Bedtime"
-                  ? "border-blue-800 bg-blue-100"
-                  : "border-gray-700 bg-gray-100 hover:bg-green-200"
-              }  outline-2 p-1 w-32 rounded-full text-md font-sans`}
-              name="measure"
-              value="At Bedtime"
-              onClick={handleChange}
-            >
-              At Bedtime
-            </button>
-            <button
-              type="button"
-              className={`border ${
-                inputState.measure === "Fasting"
-                  ? "border-blue-800 bg-blue-100"
-                  : "border-gray-700 bg-gray-100 hover:bg-green-200"
-              }  outline-2 p-1 w-32 rounded-full text-md font-sans`}
-              name="measure"
-              value="Fasting"
-              onClick={handleChange}
-            >
-              Fasting
-            </button>
-          </div>
-          <button
-            type="submit"
-            className="border border-black p-2 w-60 bg-cyan-700 text-white rounded-md text-lg hover:bg-cyan-800 font-sans"
-          >
-            Add Readings
-          </button>
-        </form>
-      </div> */}
     </>
   );
 };
