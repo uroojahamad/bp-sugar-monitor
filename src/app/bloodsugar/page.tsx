8d8b0ed55d52f2fbabae004ee94fb7b61bd3eb73
Urooj Ahmad <uroojahamad786@gmail.com>
2023-09-16 11:40:41 +0530

    added a loading state

--- src/app/bloodsugar/page.tsx
+++ src/app/bloodsugar/page.tsx
@@ -18,6 +18,7 @@ const Sugar = () => {
 
   //State for Modal box
   const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleModalBoxOpen = () => {
     setIsOpen(true);
@@ -29,11 +30,13 @@ const Sugar = () => {
 
   //Get data from supabase
   const getSugarData = async () => {
+    setIsLoading(true);
     const { data, error } = await supabase
       .from("bloodsugar")
       .select(`*`)
       .order("id", { ascending: false });
     if (data) setSugarReading(data);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -51,23 +54,37 @@ const Sugar = () => {
               : "overflow-y-auto no-scrollbar"
           } items-center h-screen bg-slate-100 rounded-2xl max-w-full max-h-full`}
         >
-          <button
-            className="w-full flex justify-center items-center p-2 space-x-4 font-sans font-bold text-white rounded-full shadow-lg px-9 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90 hover:shadow-lg border transition hover:-translate-y-0.5 duration-150 mb-3"
-            onClick={handleModalBoxOpen}
-          >
-            Add
-          </button>
-          {sugarReading.length === 0 ? (
-            <div className="w-full p-5 md:p-10">
-              <div className="flex flex-col justify-between items-center gap-10">
-                <div className="text-lg font-extralight">
-                  <p className="text-center">No records to show</p>
-                  <p>Press + to add your records</p>
-                </div>
-              </div>
+          {isLoading ? (
+            <div
+              className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status"
+            >
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                Loading...
+              </span>
             </div>
           ) : (
-            <DisplayBloodSugarDetails sugarReading={sugarReading} />
+            <>
+              <button
+                className="w-full flex justify-center items-center p-2 space-x-4 font-sans font-bold text-white rounded-full shadow-lg px-9 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90 hover:shadow-lg border transition hover:-translate-y-0.5 duration-150 mb-3"
+                onClick={handleModalBoxOpen}
+              >
+                Add
+              </button>
+
+              {sugarReading.length === 0 ? (
+                <div className="w-full p-5 md:p-10">
+                  <div className="flex flex-col justify-between items-center gap-10">
+                    <div className="text-lg font-extralight">
+                      <p className="text-center">No records to show</p>
+                      <p>Press + to add your records</p>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <DisplayBloodSugarDetails sugarReading={sugarReading} />
+              )}
+            </>
           )}
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ export type Reading = {
 
 export default function Home() {
   const [bpReading, setBpReading] = useState<Reading[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   //State for Modal box
   const [isOpen, setIsOpen] = useState(false);
@@ -31,11 +32,13 @@ export default function Home() {
 
   //Get blood pressure data from supabase
   const getBPData = async () => {
+    setIsLoading(true);
     const { data } = await supabase
       .from("bp")
       .select(`*`)
       .order("id", { ascending: false });
     if (data) setBpReading(data);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -51,25 +54,38 @@ export default function Home() {
             bpReading.length === 0
               ? "justify-center"
               : "overflow-y-auto no-scrollbar"
-          } items-center h-screen bg-slate-100 rounded-2xl max-w-full max-h-full border border-black p-3`}
+          } items-center h-screen bg-slate-100 rounded-2xl max-w-full max-h-full p-3`}
         >
-          <button
-            className="w-full flex justify-center items-center p-2 space-x-4 font-sans font-bold text-white rounded-full shadow-lg px-9 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90 hover:shadow-lg border transition hover:-translate-y-0.5 duration-150 mb-3"
-            onClick={handleModalBoxOpen}
-          >
-            Add
-          </button>
-          {bpReading.length === 0 ? (
-            <div className="w-full p-5 md:p-10">
-              <div className="flex flex-col justify-between items-center gap-10">
-                <div className="text-lg font-extralight">
-                  <p className="text-center">No records to show</p>
-                  <p>Press + to add your records</p>
-                </div>
-              </div>
+          {isLoading ? (
+            <div
+              className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status"
+            >
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                Loading...
+              </span>
             </div>
           ) : (
-            <DisplayBPDetails bpReading={bpReading} />
+            <>
+              <button
+                className="w-full flex justify-center items-center p-2 space-x-4 font-sans font-bold text-white rounded-full shadow-lg px-9 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90 hover:shadow-lg border transition hover:-translate-y-0.5 duration-150 mb-3"
+                onClick={handleModalBoxOpen}
+              >
+                Add
+              </button>
+              {bpReading.length === 0 ? (
+                <div className="w-full p-5 md:p-10">
+                  <div className="flex flex-col justify-between items-center gap-10">
+                    <div className="text-lg font-extralight">
+                      <p className="text-center">No records to show</p>
+                      <p>Press + to add your records</p>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <DisplayBPDetails bpReading={bpReading} />
+              )}
+            </>
           )}
         </div>
       </div>
